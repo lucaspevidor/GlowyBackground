b392201f56fda35c9db3a10b84134f6ff104a564
Lucas Pevidor <lucas.pevidor@gmail.com>
2023-09-20 02:12:40 -0300

    Added shadowBlur to circle draw function

--- src/Canvas/CanvasDrawer.ts
+++ src/Canvas/CanvasDrawer.ts
@@ -7,6 +7,9 @@ class CanvasDrawer {
   DrawCircle(position: Vector, radius: number, color: string) {
     const { c } = this.cm;
 
+    c.shadowColor = color;
+    c.shadowBlur = 100;
+
     c.fillStyle = color;
     c.beginPath();
     c.ellipse(position.x, position.y, radius, radius, 0, 0, 2 * Math.PI);
